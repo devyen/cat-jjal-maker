558b6b0239cd3e0a6dc7accb8a6f91a69bb7e5d2
YeEun <codpdms@naver.com>
2022-04-25 23:06:30 +0900

    Style: js -> tsx 로 수정

--- cat-jjal-maker-cra/src/App.tsx
+++ cat-jjal-maker-cra/src/App.tsx
@@ -15,7 +15,7 @@ const jsonLocalStorage = {
 };
 
 const fetchCat = async (text) => {
-  const OPEN_API_DOMAIN = "https://cataas.com";
+  const OPEN_API_DOMAIN: string = "https://cataas.com";
   const response = await fetch(`${OPEN_API_DOMAIN}/cat/says/${text}?json=true`);
   const responseJson = await response.json();
   return `${OPEN_API_DOMAIN}/${responseJson.url}`;
